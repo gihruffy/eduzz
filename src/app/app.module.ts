821bd5ae4984f3e6c2e19f79f5a6fcc8e0f907da
Giovanni Martinelli <giovanni.artica@gmail.com>
2017-12-14 23:27:52 -0200

    Criando Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
